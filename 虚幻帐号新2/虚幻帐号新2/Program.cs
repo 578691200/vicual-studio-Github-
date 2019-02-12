9000d2da1216de9792ea27f0847a4b5e4168383f
立天 陈 <578691200@qq.com>
2019-02-12 15:19:50 +0800

    class

---
+++ 虚幻帐号新2/虚幻帐号新2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 虚幻帐号新2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 虚幻帐号新2/虚幻帐号新2/person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 虚幻帐号新2
+{
+    class persone
+    {
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}

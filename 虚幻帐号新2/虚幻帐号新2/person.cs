3adaff2fe5488316dd496f4c05cda23e9d071b01
立天 陈 <578691200@qq.com>
2019-02-12 15:34:26 +0800

    15:43

--- 虚幻帐号新2/虚幻帐号新2/person.cs
+++ 虚幻帐号新2/虚幻帐号新2/person.cs
@@ -6,6 +6,7 @@ namespace 虚幻帐号新2
 {
     class persone
     {
+        public string name { get; set }
         public override bool Equals(object obj)
         {
             return base.Equals(obj);
